405955a7e635d6836ea98ab7e6634b2b51f0aeeb
Ferchitol0577 <1225100997.lfch@gmail.com>
2026-09-25 14:45:08 -0600

    Metodo que suma los valores de un arreglo usando recursividad

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -12,7 +12,21 @@ public class Recursividad {
         }
     }
 
-    /**
+ 
+      /**
+    *Funcion que realiza la suma de los numeros de un arreglo usando recursividad.
+    *Fecha: 25 Septiembre
+    *Autor: Luis Hernández
+    **/
+    public static int sumaRecursiva(int[] datos, int n) {
+    if (n <= 0) {
+        return 0;
+    } else {
+        return datos[n - 1] + sumaRecursiva(datos, n - 1);
+    }
+    }
+
+     /**
     *Funcion que realiza cuenta regresiva de acuerdo a un valor entero.
     *Fecha: 25 Septiembre
     *Autor: Luis Hernández
@@ -29,5 +43,8 @@ public class Recursividad {
     public static void main(String[] a) {
         saludo(10, "Fernando");
         cuentaRegresiva(100);
+        int[] datos = {3,4,5,6,7,8};
+        int resultado = sumaRecursiva(datos, datos.length);
+        System.out.println("La suma es: " + resultado);
     }
 }
